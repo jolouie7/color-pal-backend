6d780b6fe3be77878f8861bbe5f58016dce92d62
Your_Name <joseph161216@gmail.com>
2020-09-11 18:31:08 -0700

    create and migrate migrations

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import (User, Palette)
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = "__all__"
 
-class PaletteSerializer(serializers.Serializer):
+
+class PaletteSerializer(serializers.ModelSerializer):
   class Meta:
     model = Palette
     fields = "__all__"
